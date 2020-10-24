46e9b2da698cdf97e56d44fbedaf9f78ec689cf8
Leó Fannar Sævarsson <leo@programm.is>
2020-10-24 18:28:14 +0000

    update our menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div >
+     <HomePage></HomePage>
+    </div>
+  );
 }
 
 export default App;
