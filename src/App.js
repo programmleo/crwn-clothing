a2803173a3aedb70d6826f2755f448a19894933d
Leó Fannar Sævarsson <leo@programm.is>
2020-10-25 12:13:05 +0000

    Add shop page, collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import {Route} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
-    <div >
-     <HomePage></HomePage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
