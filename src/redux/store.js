84b98ab65b7695b5be42a87a124e1c71ff9c67d2
Leó Fannar Sævarsson <leo@programm.is>
2020-10-28 22:26:21 +0000

    adding redux-persistore /localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import {createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import roodReducer from './root-reducer';
 
-const middleware = [logger];
+import rootReducer from './root-reducer';
 
-const store = createStore(roodReducer, applyMiddleware(...middleware));
+const middlewares = [logger];
 
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor = persistStore(store);
